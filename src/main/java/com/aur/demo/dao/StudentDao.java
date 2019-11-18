7c96bff072858393cfee867c97b8f4375b51f111
aur <agheorghet@gmail.com>
2019-11-18 12:25:31 +0200

    Vs1

---
+++ src/main/java/com/aur/demo/dao/StudentDao.java
@@ -0,0 +1,13 @@
+package com.aur.demo.dao;
+
+import com.aur.demo.model.Student;
+import java.util.List;
+import java.util.UUID;
+
+public interface StudentDao {
+    int insertNewStudent(UUID studentId, Student student);
+    Student selectStudentById(UUID studentId);
+    List<Student>selectAllStudents();
+    int updateStudentById(UUID studentId, Student studentUpdate);
+    int deleteStudentById(UUID studentId);
+}
